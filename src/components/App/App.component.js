beef13f0334929db01fb835bc7bfdc68977b850f
silpagui <silpagui.sa@gmail.com>
2022-02-01 22:12:18 +0100

    feat(App): implement react-router-dom

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.styles.css";
+import { Home } from "../Home/Home.component";
+import { CountryDetails } from "../CountryDetails/CountryDetails.component";
 
 export function App() {
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
+    <div className="container p-4">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path=":countryName" element={<CountryDetails />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
