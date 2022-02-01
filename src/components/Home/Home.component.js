22f42214acf208f41636a0b18fadab4130fc0bd6
silpagui <silpagui.sa@gmail.com>
2022-02-01 22:07:48 +0100

    feat(Home): implement Home component

---
+++ src/components/Home/Home.component.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { Header } from "../Header/Header.component";
+import { CountriesList } from "../CountriesList/CountriesList.component";
+
+export function Home() {
+  const [countriesList, setCountriesList] = useState([]);
+  const [searchCountry, setSearchCountry] = useState("");
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountriesList(data);
+      });
+  }, [setCountriesList]);
+
+  function handleInputOnChange(event) {
+    setSearchCountry(event.target.value);
+  }
+
+  const search = searchCountry.toLowerCase();
+  const filteredCountry = countriesList.filter((country) =>
+    country.name.common.toLowerCase().includes(search)
+  );
+
+  return (
+    <div>
+      <Header handleInputOnChange={handleInputOnChange} />
+      <CountriesList filteredCountry={filteredCountry} />
+    </div>
+  );
+}
