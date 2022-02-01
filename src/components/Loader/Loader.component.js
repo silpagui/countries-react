3738551ae145733ede4e0479b425307e8894f6c4
silpagui <silpagui.sa@gmail.com>
2022-02-01 23:22:06 +0100

    feat(Loader): create component

---
+++ src/components/Loader/Loader.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Loader() {
+  return (
+    <div className="spinner-border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
+}
