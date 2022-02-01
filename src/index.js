16f899031efdb8343399c44083ebfe3d63b5baa8
silpagui <silpagui.sa@gmail.com>
2022-02-01 22:06:43 +0100

    feat(index): implement bootstrap and fonts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom";
 import { App } from "./components/App/App.component";
 
