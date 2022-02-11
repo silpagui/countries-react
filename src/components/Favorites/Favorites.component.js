bad985e7525441c5401cdfb5dee247e1d307a2a3
silpagui <silpagui.sa@gmail.com>
2022-02-11 15:46:23 +0100

    feat(Favorites): create component

---
+++ src/components/Favorites/Favorites.component.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  deleteAllFavorites,
+  deleteFavorite,
+} from "../../store/actions/favorites.actions";
+
+export function Favorites() {
+  const dispatch = useDispatch();
+
+  const countries = useSelector((store) => store.countries.data);
+  const favorites = useSelector((store) => store.favorites.favorites);
+
+  const favoritesFiltered = countries.filter((country) =>
+    favorites.find((fav) => fav === country.name.common)
+  );
+
+  return (
+    <div>
+      <h2>Favorites Countries</h2>
+      <div className="row row-cols-1 row-cols-md-6 g-4 my-3">
+        {favorites.length ? (
+          favoritesFiltered.map((country) => {
+            return (
+              <div className="col" key={country.name.common}>
+                <div className="card h-100">
+                  <img
+                    src={country?.flags?.svg}
+                    alt={`${country.name.common} Flag`}
+                    className="card-img-top"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{country.name.common}</h5>
+                    <p className="card-text">Capital: {country?.capital}</p>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={() => {
+                        dispatch(deleteFavorite(country.name.common));
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <p>No favorites Countries Selected</p>
+        )}
+      </div>
+      <div className="container">
+        {favorites.length ? (
+          <button
+            type="button"
+            className="btn btn-outline-danger"
+            onClick={() => {
+              dispatch(deleteAllFavorites());
+            }}
+          >
+            Remove All Favorites
+          </button>
+        ) : (
+          ""
+        )}
+
+        <Link to="/" className="btn btn-primary">
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+}
