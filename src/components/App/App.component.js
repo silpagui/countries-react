2b84030af164a43a78c08cadf5d2578217be851c
silpagui <silpagui.sa@gmail.com>
2022-02-11 15:54:48 +0100

    feat(App): add route to favorites

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../Home/Home.component";
 import { CountryDetails } from "../CountryDetails/CountryDetails.component";
+import { Favorites } from "../Favorites/Favorites.component";
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path=":countryName" element={<CountryDetails />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
